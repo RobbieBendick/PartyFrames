12ef1f3ac0afaeb01842be8bbe2f58af2a196a25
Robbie Bendick <robbiebendick@gmail.com>
2021-04-23 15:10:14 -0700

    change border color

--- core.lua
+++ core.lua
@@ -7,11 +7,18 @@ local function eventHandler(self,event,...)
 local partyMemberFrames = {
     PartyMemberFrame1, PartyMemberFrame2, PartyMemberFrame3, PartyMemberFrame4
 }
+local partyMemberFrameTextures = {
+    PartyMemberFrame1Texture, PartyMemberFrame2Texture, PartyMemberFrame3Texture, PartyMemberFrame4Texture
+}
+
     local members = GetNumGroupMembers()
     if members > 1 then
         ShowPartyFrame();
         for i=1,#partyMemberFrames do
             partyMemberFrames[i]:SetScale(1.3)
+        end
+        for i=1,#partyMemberFrameTextures do
+            partyMemberFrameTextures[i]:SetVertexColor(0,0,0)
         end
     end
 end
