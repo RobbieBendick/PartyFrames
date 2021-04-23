46a85d86b3113e5c5e7c01e497e09d13b9913234
Robbie Bendick <robbiebendick@gmail.com>
2021-04-23 15:40:54 -0700

    hide raidframe manager

--- core.lua
+++ core.lua
@@ -2,7 +2,6 @@ local frame = CreateFrame("FRAME", "PartyFrame")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 
-
 local function eventHandler(self,event,...)
 local partyMemberFrames = {
     PartyMemberFrame1, PartyMemberFrame2, PartyMemberFrame3, PartyMemberFrame4
@@ -10,15 +9,15 @@ local partyMemberFrames = {
 local partyMemberFrameTextures = {
     PartyMemberFrame1Texture, PartyMemberFrame2Texture, PartyMemberFrame3Texture, PartyMemberFrame4Texture
 }
-
     local members = GetNumGroupMembers()
     if members > 1 then
         ShowPartyFrame();
+        CompactRaidFrameManager:Hide();
         for i=1,#partyMemberFrames do
             partyMemberFrames[i]:SetScale(1.3)
         end
         for i=1,#partyMemberFrameTextures do
-            partyMemberFrameTextures[i]:SetVertexColor(0,0,0)
+            partyMemberFrameTextures[i]:SetVertexColor(0.1,0.1,0.1)
         end
     end
 end
